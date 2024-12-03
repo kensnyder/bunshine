73378cd4721c707dc21835500b357495fa35ce3f
Ken Snyder <kendsnyder@gmail.com>
2024-12-02 23:03:47 -0700

    [remix-adapter-bunshine] vite dev server working

---
+++ packages/remix-adapter-bunshine/src/connectToBunshine.ts
@@ -0,0 +1,21 @@
+import { type Middleware } from 'bunshine';
+import { connectToFetch, type FlatHandlers } from 'connect-to-fetch';
+
+export default function connectToBunshine(
+  ...connectHandlers: FlatHandlers[]
+): Middleware {
+  const getResponse = connectToFetch(...connectHandlers);
+  return async function (c) {
+    try {
+      return await getResponse(c.request);
+    } catch (e) {
+      const error = e as Error;
+      if (error.message === 'UNHANDLED') {
+        // handler called next(), but there are no other connect handlers
+      } else {
+        // send off to our 500 error handler
+        throw error;
+      }
+    }
+  };
+}
